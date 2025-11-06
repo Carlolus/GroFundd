1307420c93c508220301ca7477fe927507a0f36a
Carlos Córdoba <carlosc17@udenar.edu.co>
2025-11-06 10:12:52 -0500

    Added budget service

---
+++ frontend/src/app/core/services/budget.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Budget } from '../interfaces/budget.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BudgetService {
+  private apiUrl = 'http://localhost:3000/budgets';
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getBudgets(){
+    return this.http.get<Budget[]>(`${this.apiUrl}`);
+  }
+
+  createBudget(budget: Budget){
+    return this.http.post<Budget>(`${this.apiUrl}`,budget);
+  }
+
+  updateBudget(id: string, updatedBudget: Budget){
+    return this.http.put<Budget>(`${this.apiUrl}/${id}`,updatedBudget);
+  }
+
+  deleteBudget(id: string){
+    return this.http.delete<Budget>(`${this.apiUrl}/${id}`);
+  }
+}
